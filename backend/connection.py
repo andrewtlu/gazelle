194a7dd2c3a46f270d0c12922840e80d900ab95b
andrewtlu <andrewlulu2012@gmail.com>
2026-03-14 21:53:11 -0400

    Add connection start logic

--- backend/connection.py
+++ backend/connection.py
@@ -16,6 +16,25 @@ class Connection:
     def is_connected(self) -> bool:
         return self.process is not None and self.process.returncode is None
 
+    async def start(
+        self, config_path: Path, password_file: Path, log_callback: Callable[[], str], exit_callback: Callable[[], any]
+    ):
+        if self.is_connected:
+            await self.stop()
+
+        self.config_name = config_path.stem
+        self.process = await asyncio.create_subprocess_exec(
+            "sudo",
+            "openvpn",
+            "--config",
+            str(config_path),
+            "--auth-user-pass",
+            str(password_file),
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.STDOUT,  # stderr -> stdout -> pipe
+        )
+        asyncio.create_task(self.listen(log_callback, exit_callback))
+
     async def listen(self, log_callback: Callable[[], str], exit_callback: Callable[[], any]):
         """Establishes a stream between the current process' output and gazelle's TUI."""
         assert self.process and self.process.stdout
